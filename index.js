8f7e61863418a3b91384b1987913e770b76d5ebe
Rayhan-Rony <jobayerahmedrony@gmail.com>
2022-11-30 04:22:27 +0600

    added delete method for buyer and seller

--- index.js
+++ index.js
@@ -112,6 +112,12 @@ async function run() {
             const result = await usersCollection.find(query).toArray()
             res.send(result)
         })
+        app.delete('/user/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await usersCollection.deleteOne(query)
+            res.send(result)
+        })
 
     }
     finally {
